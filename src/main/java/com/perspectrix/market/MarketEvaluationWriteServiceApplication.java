8734362e05571f98415006384bad738c5f605f06
william.stone <will@williamstone.me>
2025-01-20 13:27:53 -0500

    Initialized Spring Boot app

---
+++ src/main/java/com/perspectrix/market/MarketEvaluationWriteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.perspectrix.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketEvaluationWriteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketEvaluationWriteServiceApplication.class, args);
+	}
+
+}
